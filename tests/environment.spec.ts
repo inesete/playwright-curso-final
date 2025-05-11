06a1bd50d739432a7357530efcfe43dcd6922d9a
inesete <ialonsolop@gmail.com>
2025-05-11 11:28:47 +0200

    fix nondefined error

--- tests/environment.spec.ts
+++ tests/environment.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('environment test', async ({ page }) => {
-    await page.goto(process.env.URL);
+    await page.goto(process.env.URL ?? '');
     await page.pause();
 });
